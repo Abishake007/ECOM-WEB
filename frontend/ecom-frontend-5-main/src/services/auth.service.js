8cd5c47e37a198d2587fb494601352dd950b61df
Abishake007 <abishakearunagirinathan16@gmail.com>
2026-05-11 15:56:46 +0530

    sign in error fix

--- frontend/ecom-frontend-5-main/src/services/auth.service.js
+++ frontend/ecom-frontend-5-main/src/services/auth.service.js
@@ -1,6 +1,6 @@
-import axios from "axios";
+import API from "../axios";
 
-const API_URL = "http://localhost:8080/api/auth/";
+const API_URL = "/auth/";
 
 const register = (username, email, password) => {
   return axios.post(API_URL + "signup", {
@@ -12,7 +12,7 @@ const register = (username, email, password) => {
 
 // services/auth.service.js
 const login = (username, password) => {
-  return axios
+  return API
     .post(API_URL + "signin", { username, password })
     .then((response) => {
       // 1. Check if the response actually exists
