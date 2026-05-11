e4c743d924629bb6d2603958bfbf7b3278d71095
Abishake007 <abishakearunagirinathan16@gmail.com>
2026-05-11 15:24:01 +0530

    cors config fixing

--- backend/ecom-proj/src/main/java/com/telusko/ecom_proj/security/WebSecurityConfig.java
+++ backend/ecom-proj/src/main/java/com/telusko/ecom_proj/security/WebSecurityConfig.java
@@ -58,7 +58,7 @@ public class WebSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173","https://your-app.vercel.app"));
+        configuration.setAllowedOrigins(Arrays.asList("https://ecom-web-vert.vercel.app","http://localhost:5173"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
